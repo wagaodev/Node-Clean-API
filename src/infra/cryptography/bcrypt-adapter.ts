d7eef44504a4a36b873e8093def20bbd27e8a4ec
Wagão <wcfx.dev@gmail.com>
2024-08-10 04:28:54 -0300

    feat: ensure bcryptAdapter returns a hash

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -7,7 +7,7 @@ export class BcryptAdapter implements EncrypterProtocol {
     this.salt = salt;
   }
   async encrypt(value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt);
-    return "";
+    const hash = await bcrypt.hash(value, this.salt);
+    return hash;
   }
 }
