654aa5e410648483d0972b8122c16d637336d8e3
Wagão <wcfx.dev@gmail.com>
2024-08-10 04:31:13 -0300

    refactor: create a makesut

--- src/infra/cryptography/bcrypter-adapter.spec.ts
+++ src/infra/cryptography/bcrypter-adapter.spec.ts
@@ -7,16 +7,19 @@ jest.mock("Bcrypt", () => ({
   },
 }));
 describe("Bcrypt Adapter", () => {
+  const salt = 12;
+
+  const makeSut = (): BcryptAdapter => {
+    return new BcryptAdapter(salt);
+  };
   it("Should call bcrypt with correct value", async () => {
-    const salt = 12;
-    const sut = new BcryptAdapter(salt);
+    const sut = makeSut();
     const hashSpy = jest.spyOn(bcrypt, "hash");
     await sut.encrypt("any_value");
     expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
   });
   it("Should return a hash on success", async () => {
-    const salt = 12;
-    const sut = new BcryptAdapter(salt);
+    const sut = makeSut();
     const hash = await sut.encrypt("any_value");
     expect(hash).toBe("hash");
   });
