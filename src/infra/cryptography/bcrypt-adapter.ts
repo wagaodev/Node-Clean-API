9fba36cd82986f1b565f47a03aa9595908a7a3ff
Wagão <wcfx.dev@gmail.com>
2024-08-10 04:22:13 -0300

    feat: ensure bcryptAdapter calls bcrypt with correct values

---
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+import { EncrypterProtocol } from "../../data/protocols/encrypter";
+
+export class BcryptAdapter implements EncrypterProtocol {
+  private readonly salt: number;
+  constructor(salt: number) {
+    this.salt = salt;
+  }
+  async encrypt(value: string): Promise<string> {
+    await bcrypt.hash(value, this.salt);
+    return "";
+  }
+}
