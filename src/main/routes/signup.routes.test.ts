a12f9af2d6a9179bb013a0414a6b36b7e129afc7
Wagão <wcfx.dev@gmail.com>
2024-09-03 01:12:25 -0300

    refactor: connect to mongoDB before integration test

---
+++ src/main/routes/signup.routes.test.ts
@@ -0,0 +1,27 @@
+import request from "supertest";
+import app from "../config/app";
+import { MongoHelper } from "../../infra/db/mongodb/account-repository/helper/mongo-helper";
+
+describe("Signup Routes", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection("accounts");
+    accountCollection.deleteMany({});
+  });
+
+  it("Should return an account on success", async () => {
+    await request(app).post("/api/signup").send({
+      name: "Wagão",
+      email: "wagaodev@gmail.com",
+      password: "123",
+      passwordConfirmation: "123",
+    });
+  });
+});
