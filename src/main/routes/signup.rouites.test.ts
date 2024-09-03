2c99570c682890b3f45ff2f6f8e6b98f736a602e
Wagão <wcfx.dev@gmail.com>
2024-09-03 00:39:40 -0300

    feat: ensure signup route returns an account on success

---
+++ src/main/routes/signup.rouites.test.ts
@@ -0,0 +1,13 @@
+import request from "supertest";
+import app from "../config/app";
+
+describe("Signup Routes", () => {
+  it("Should return an account on success", async () => {
+    await request(app).post("/api/signup").send({
+      name: "Wagão",
+      email: "wagaodev@gmail.com",
+      password: "123",
+      passwordConfirmation: "123",
+    });
+  });
+});
