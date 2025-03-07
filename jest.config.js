858767392f3eb64c047b8cafe633b651f9f8274f
Wagner Goulart <wagner.goulart.3@globalhitss.com.br>
2025-03-07 00:57:29 -0300

    chore: add new instructions in our coverage config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ["<rootDir>/src"],
   preset: "@shelf/jest-mongodb",
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**"],
   coverageDirectory: "coverage",
   testEnvironment: "node",
   testPathIgnorePatterns: ["dist", "node_modules"],
